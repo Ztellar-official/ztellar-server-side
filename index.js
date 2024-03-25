5cf97adcfaddb7ee73ced775ddeb2ad7c9f5f016
Ztellar-official <164864963+Ztellar-official@users.noreply.github.com>
2024-03-25 22:17:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin:["https://ztellar-client-side.onrender.com"],
+    origin:["https://vizcom.onrender.com"],
     credentials:true
 }));
 
